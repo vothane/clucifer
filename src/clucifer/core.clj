92230cfb8dac5ca5983e23b6c53951bd496106da
Thane Vo <vothane@gmail.com>
2014-04-06 22:42:10 -0600

    add stuff from clucy that doesn't need refactoring

--- src/clucifer/core.clj
+++ src/clucifer/core.clj
@@ -14,3 +14,18 @@
            (org.apache.lucene.util Version AttributeSource)
            (org.apache.lucene.store NIOFSDirectory RAMDirectory Directory)))
 
+(def ^{:dynamic true} *version* Version/LUCENE_CURRENT)
+(def ^{:dynamic true} *analyzer* (StandardAnalyzer. *version*))
+
+;; flag to indicate a default "_content" field should be maintained
+(def ^{:dynamic true} *content* true)
+
+(defn memory-index
+  "Create a new index in RAM."
+  []
+  (RAMDirectory.))
+
+(defn disk-index
+  "Create a new index in a directory on disk."
+  [^String dir-path]
+  (NIOFSDirectory. (File. dir-path)))

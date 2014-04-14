6e3c0ecb512df4e4e0dfeb3f2d2103206ac5bd86
Thane Vo <vothane@gmail.com>
2014-04-14 03:57:51 -0600

    save work

--- src/clucifer/index.clj
+++ src/clucifer/index.clj
@@ -1,9 +1,20 @@
 (ns clucifer.index
-  (:import (org.apache.lucene.document Document Field Field$Index Field$Store)))
+  (:require [clojure.field :refer :all])
+  (:import (org.apache.lucene.document Document Field Field$Index Field$Store)
+           (org.apache.lucene.index IndexWriter IndexReader Term
+                                    IndexWriterConfig DirectoryReader FieldInfo)
+           (org.apache.lucene.analysis.standard StandardAnalyzer)
+           (org.apache.lucene.util Version AttributeSource)))
+
+(def ^{:dynamic true} *version* Version/LUCENE_CURRENT)
+(def ^{:dynamic true} *analyzer* (StandardAnalyzer. *version*))
 
 (defn create-index []
-  (let [index (ref {:index-info {}, :uncommitted {}, :deleted-ids []})]
+  (let [index (ref {:index-info {}, :uncommitted {}})]
     index))
+
+(defn- index-writer ^IndexWriter [index]
+  (IndexWriter. index (IndexWriterConfig. *version* *analyzer*)))
 
 (defn add [index data]
   (dosync (alter index merge-with merge {:uncommitted data})))
@@ -11,8 +22,23 @@
 (defn clear [index]
   (dosync (alter index update-in [:uncommitted] {})))
 
+(defn- add-field [document map & meta-map]
+  (let [base  (create-field)
+        field (update-meta-data field attribute value)]
+    (.add ^Document document (to-lucene-field field))))
+
+(defn- map->document [map]
+  (let [document (Document.)]
+    (for [pair map] (add-field document pair (meta pair)))))
+
+(defn- add->lucence [index & maps]
+  (with-open [writer (index-writer index)]
+    (doseq [m maps]
+      (.addDocument writer (map->document m)))))
+
 (defn commit [index]
-  (dosync (alter index update-in [:uncommitted] {})))
+  (add->lucence index (:uncommitted @index))
+  (clear index))
 
 (defn uncommitted [index]
   (:uncommitted @index))
--- test/clucifer/index_test.clj
+++ test/clucifer/index_test.clj
@@ -5,7 +5,7 @@
 (deftest index
    
   (def test-index (create-index)) 
-  (add test-index {:id "7", :team "Red Sox"})
+  (add test-index {:id 7, :team "Red Sox"})
 
   (testing "should be empty after clear"
     (is (= (clear test-index) 0)))
@@ -14,6 +14,8 @@
     (is (= (commit test-index) 0)))
 
   (testing "contains one uncommited document"
-    (is (= (count (uncommitted test-index)) 1)))  
-
+    (is (= (count (uncommitted test-index)) 1))
+    (is (= (:id (uncommitted test-index)) 7))
+    (is (= (:team (uncommitted test-index)) "Red Sox"))
+    )  
 )             

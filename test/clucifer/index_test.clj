efca6ae1157dc704b1ee25a6898bb3d98239af05
Thane Vo <vothane@gmail.com>
2014-04-15 21:18:00 -0600

    test for searching after committing data to Lucene

--- test/clucifer/index_test.clj
+++ test/clucifer/index_test.clj
@@ -1,11 +1,12 @@
 (ns clucifer.index-test
   (:require [clojure.test :refer :all]
+            [clucifer.core :refer :all]
             [clucifer.index :refer :all]))
 
 (deftest index
    
   (def test-index (create-index)) 
-  (add test-index {:id 7, :team "Red Sox"})
+  (add test-index {"id" 7, "team" "Red Sox"})
 
   (testing "should be empty after clear"
     (is (= (clear test-index) 0)))
@@ -16,6 +17,11 @@
   (testing "contains one uncommited document"
     (is (= (count (uncommitted test-index)) 1))
     (is (= (:id (uncommitted test-index)) 7))
-    (is (= (:team (uncommitted test-index)) "Red Sox"))
-    )  
+    (is (= (:team (uncommitted test-index)) "Red Sox")))
+
+  (commit test-index)
+  
+  (testing "should have successfully indexed uncommitted data"
+    (let [hits (search test-index "team" "Red Sox")]
+    (is (= (count (hits)) 1))))
 )             

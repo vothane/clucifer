2a6c918da73fc283572b14b68684e7ad53ed62a1
Thane Vo <vothane@gmail.com>
2014-07-14 14:15:37 -0600

    add test for macro that adds an index

--- test/clucifer/core_test.clj
+++ test/clucifer/core_test.clj
@@ -1,3 +1,8 @@
 (ns clucifer.core-test
   (:require [clojure.test :refer :all]
             [clucifer.core :refer :all]))
+
+(testing "lucene macro"
+  (let [index (deflucence *instance*)]
+    (is (= index "org.apache.lucene.store.RAMDirectory"))))
+
